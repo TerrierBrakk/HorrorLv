c91373164b42c67c407d499d17dd4a535f2adf06
SorenGedeon <32682025+TerrierBrakk@users.noreply.github.com>
2020-02-04 09:12:11 -0600

    p

---
+++ Assets/Scripts/CastingPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastingPlayer : MonoBehaviour
+
+{
+    public static float DistanceFromTarget;
+    public float ToTarget;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+     //Calculamos la distancia del objetivo con respecto al jugador
+        if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out hit))
+        {
+            ToTarget = hit.distance;
+            DistanceFromTarget = ToTarget;
+        }
+    }
+}
---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorOpen : MonoBehaviour
+{
+    public float Distance;
+    public GameObject objActionDisplay;
+    public GameObject objText;
+    public GameObject door;
+    public GameObject CrossExtra;
+    
+    public AudioSource doorsound;
+    
+    
+
+// Update is called once per frame
+    void Update()
+    {
+        Distance = CastingPlayer.DistanceFromTarget;
+    }
+
+    void OnMouseOver()
+    {
+        if (Distance <= 1.0f)
+        {
+            CrossExtra.SetActive(true);
+            objActionDisplay.SetActive(true);
+            objText.SetActive(true);
+        }
+        else
+        {
+            CrossExtra.SetActive(false);
+            objActionDisplay.SetActive(false);
+            objText.SetActive(false);
+        }
+        if(Input.GetButtonDown("Action"))
+        {
+            if (Distance <= 2.0f)
+            {
+                this.GetComponent<BoxCollider>().enabled = false;
+                objActionDisplay.SetActive(false);
+                objText.SetActive(false);
+                door.GetComponent<Animation>().Play("AbrirPuerta");
+                doorsound.Play();
+            }
+        }
+    }
+
+    void OnMouseExit()
+    {
+            CrossExtra.SetActive(false);
+            objActionDisplay.SetActive(false);
+            objText.SetActive(false);
+        
+    }
+    
+}
